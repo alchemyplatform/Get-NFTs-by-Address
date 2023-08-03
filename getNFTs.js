f624c3e38f794acf2a81611be4befc12142921cd
jrp2212 <72630123+jrp2212@users.noreply.github.com>
2023-08-03 14:48:15 -0400

    Add files via upload

---
+++ getNFTs.js
@@ -0,0 +1,31 @@
+// Import necessary modules
+const { Alchemy, Network } = require("alchemy-sdk");
+require('dotenv').config();
+
+// Configure Alchemy SDK with API key and network
+const config = {
+  apiKey: process.env.API_KEY,
+  network: Network.ETH_MAINNET,
+};
+const alchemy = new Alchemy(config);
+
+
+// Main function to fetch and print NFTs for a specific owner.
+const main = async () => {
+  // Provided below is a sample address
+  // Configure accordingly
+  const address = "vitalik.eth";
+  const nfts = await alchemy.nft.getNftsForOwner(address);
+
+  // Print NFT details
+  // Refer to https://docs.alchemy.com/reference/getnfts to print more fields
+  nfts.ownedNfts.forEach((nft) => {
+    console.log("Title:", nft.title);
+    console.log("Contract Address:", nft.contract.address);
+    console.log("Token ID:", nft.tokenId);
+    console.log("Token Type:", nft.tokenType);
+    console.log("----------------------------------");
+  });
+};
+
+main(); // Run the main function
